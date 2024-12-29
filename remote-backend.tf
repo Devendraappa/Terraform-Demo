4480609d23a26762e6cebd45d73eda60833751f1
Devendraappa <devendraappa.kune@gmail.com>
2024-12-29 18:41:26 +0530

    Update remote-backend.tf

--- remote-backend.tf
+++ remote-backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "dev-terraform-789"
+    bucket         = "composition-124"
     key            = "demo/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-module"
